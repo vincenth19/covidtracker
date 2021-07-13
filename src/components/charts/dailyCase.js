7251ac24e0ce0f7f5bfcb459d73f6ba0ff2e6f9b
jogerj <30559735+jogerj@users.noreply.github.com>
2021-07-13 23:36:09 +0200

    Set travellerWidth depend on window size If window is smaller than 1000 assume mobile and use bigger traveller width for touch screens. Otherwise use smaller button for desktop windows

--- src/components/charts/dailyCase.js
+++ src/components/charts/dailyCase.js
@@ -173,7 +173,7 @@ function Chart({data, quantity}) {
                 >
                     <CartesianGrid strokeDasharray="3 3"/>
                     <Brush
-                        travellerWidth={15}
+                        travellerWidth={chartWidth() < 1000 ? 30 : 15}
                         width={chartWidth() * 0.8}
                         dataKey="tanggal"
                         x={chartWidth() * 0.1}
